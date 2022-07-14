85723df82acea207ecc4966ab518e7a1d1d58c6b
Alexandr Erofeev <61013758+ghilsidoll@users.noreply.github.com>
2022-07-14 22:55:33 +0400

    14072022.3

--- app/src/main/scala/com/github/ghilsidoll/irc/controller/ChatSceneController.scala
+++ app/src/main/scala/com/github/ghilsidoll/irc/controller/ChatSceneController.scala
@@ -1,9 +1,11 @@
 package com.github.ghilsidoll.irc.controller
 
 import akka.actor.typed.ActorSystem
+
 import com.github.ghilsidoll.irc.actor.RootBehavior
 import com.github.ghilsidoll.irc.actor.RootBehavior.{PostLogin, PostMessage}
 import com.typesafe.config.ConfigFactory
+
 import javafx.application.Platform
 import javafx.event.{Event, EventHandler}
 import javafx.fxml.{FXML, FXMLLoader}
@@ -44,18 +46,28 @@ class ChatSceneController(private val login: String) {
 
   private final var actorSystem: ActorSystem[RootBehavior.Command] = _
 
+  private var recipientSet: Set[String] = Set("Group")
+
   def getLogin: String = {
     login
   }
 
   def addRecipient(recipientLogin: String): Unit = {
-    recipientChoiceBox.getItems.add(recipientLogin)
+    recipientSet += recipientLogin
+
+    val lastValue: String = recipientChoiceBox.getValue
+
+    Platform.runLater(() => {
+      recipientChoiceBox.getItems.clear()
+      recipientSet.foreach(value => {
+        recipientChoiceBox.getItems.add(value)
+      })
+      recipientChoiceBox.setValue(lastValue)
+    })
   }
 
   def startup(port: Int, controller: ChatSceneController): Unit = {
-    val config = ConfigFactory.parseString(s"""akka.remote.artery.canonical.port=$port
-      akka.cluster.seed-nodes=["akka://chat@127.0.0.1:25251",
-      "akka://chat@127.0.0.1:25252"]""")
+    val config = ConfigFactory.parseString(s"""akka.remote.artery.canonical.port=$port""")
       .withFallback(ConfigFactory.load("application.conf"))
 
     actorSystem = ActorSystem(RootBehavior(controller), "chat", config)
@@ -65,10 +77,10 @@ class ChatSceneController(private val login: String) {
     val screenBounds = Screen.getPrimary.getVisualBounds
     val window = event.getSource.asInstanceOf[Node].getScene.getWindow
 
-    window.setX((screenBounds.getWidth - 1000) / 2)
-    window.setY((screenBounds.getHeight - 760) / 2)
+    window.setX((screenBounds.getWidth - 628) / 2)
+    window.setY((screenBounds.getHeight - 660) / 2)
 
-    window.asInstanceOf[Stage].setScene(new Scene(loader.load(), 1000, 760))
+    window.asInstanceOf[Stage].setScene(new Scene(loader.load(), 628, 660))
   }
 
    def displayMessage(login: String, content: String, modifier: Int = -1): Unit = {
--- app/src/main/scala/com/github/ghilsidoll/irc/event/SessionEvent.scala
+++ app/src/main/scala/com/github/ghilsidoll/irc/event/SessionEvent.scala
@@ -1,7 +1,5 @@
 package com.github.ghilsidoll.irc.event
 
-import akka.cluster.ClusterEvent.MemberEvent
-
 sealed trait SessionEvent
 final case class MessagePosted(message: String, from: String, to: String) extends SessionEvent
 final case class LoginPosted(login: String) extends SessionEvent
